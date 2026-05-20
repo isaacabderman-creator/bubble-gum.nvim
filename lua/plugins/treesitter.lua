0779d3a355beb10c1d57854e8f3d0cf4509c4a24
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-05-20 19:07:15 +0300

    feat(nvim): refresh treesitter setup and parser locks

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,42 @@
+local languages = {
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "bash",
+  "markdown",
+  "markdown_inline",
+  "regex",
+  "hyprlang",
+  "python",
+  "javascript",
+  "typescript",
+  "tsx",
+  "java",
+  "html",
+  "svelte",
+  "css",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { 
-          "lua", "vim", "vimdoc", "query", "bash", 
-          "markdown", "markdown_inline", "regex", "hyprlang",
-          "python",
-          "javascript",
-          "typescript",
-          "tsx",
-          "java",
-          "html",
-          "svelte",
-          "css"
-        },
-        
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        
-        -- Better indentation based on code structure
-        indent = { enable = true },
-        
+    config = function()
+      local treesitter = require("nvim-treesitter")
+
+      treesitter.setup()
+
+      local group = vim.api.nvim_create_augroup("NvimTreesitterConfig", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        pattern = languages,
+        callback = function()
+          vim.treesitter.start()
+          vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.wo.foldmethod = "expr"
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
     end,
   },
